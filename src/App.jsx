ff5b5e36445babc525052c024feac2c54bc7303c
Samuel <samuel.santyago2.ss@gmail.com>
2024-05-13 23:11:12 -0600

    feature: first commit create pokedex application

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+import LayoutHome from "./assets/pages/home/layout/LayoutHome"
+
+function App() {
+  return (
+    <>
+      <LayoutHome/>
+    </>
+  )
+}
+
+export default App
